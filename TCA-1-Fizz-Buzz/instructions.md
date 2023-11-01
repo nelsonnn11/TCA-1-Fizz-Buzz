438e19c997c234908c29928d6ae961fd24e56f02
124norellana@frhsd.com <124norellana@frhsd.com>
2023-11-01 08:51:47 -0400

    starter

---
+++ TCA-1-Fizz-Buzz/instructions.md
@@ -0,0 +1,14 @@
+# Instructions  
+## FizzBuzz
+
+Write a `fizzbuzz()` function that takes in a number, `n`, and prints out the numbers from 1 to `n`. 
+
+For **multiples of three**, replace the number with the word `"Fizz"`, and for the **multiples of five**, replace the number with the word `"Buzz"`. 
+
+For numbers that are **multiples of both three and five**, use `"FizzBuzz"` (capitalization matters).
+
+For example, `fizzbuzz(16)` should print:
+`1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16`
+
+
+*You don't need to have them separated by commas; you can have each number (or word) get printed on it's own line*
---
+++ TCA-1-Fizz-Buzz/script.js
@@ -0,0 +1,26 @@
+function fizzbuzz(n) {
+	// Write your code here
+
+
+	// Leave the console.log below at the bottom of the function
+	console.log("-------------");
+}
+
+fizzbuzz(16);
+fizzbuzz(25);
+
+
+// Instructions  
+// FizzBuzz
+
+// Write a `fizzbuzz()` function that takes in a number, `n`, and prints out the numbers from 1 to `n`. 
+
+// For **multiples of three**, replace the number with the word `"Fizz"`, and for the **multiples of five**, replace the number with the word `"Buzz"`. 
+
+// For numbers that are **multiples of both three and five**, use `"FizzBuzz"` (capitalization matters).
+
+// For example, `fizzbuzz(16)` should print:
+// `1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16`
+
+
+// *You don't need to have them separated by commas; you can have each number (or word) get printed on it's own line*
