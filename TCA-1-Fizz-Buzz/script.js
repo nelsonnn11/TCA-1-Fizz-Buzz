b1d94e57a292df04cf4f0f830ee868fc8c44274f
124norellana@frhsd.com <124norellana@frhsd.com>
2023-11-01 09:38:55 -0400

    solved

--- TCA-1-Fizz-Buzz/script.js
+++ TCA-1-Fizz-Buzz/script.js
@@ -1,5 +1,16 @@
 function fizzbuzz(n) {
 	// Write your code here
+	for (let i = 1; i <= n; i++ ){
+		if(i % 3 === 0 && i % 5 === 0){
+			console.log("FizzBuzz");
+		} else if (i % 3 === 0){
+			console.log('Fizz');
+		}else if (i % 5 === 0){
+			console.log('Buzz')
+		}else{
+			console.log(i)
+		}
+	}
 
 
 	// Leave the console.log below at the bottom of the function
